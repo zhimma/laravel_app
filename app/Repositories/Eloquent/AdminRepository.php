a3391e241c717e2ac4cf277a6d60da7e83008cc2
mma <18710839146@163.com>
2018-01-03 23:29:46 +0800

    add user table search

--- app/Repositories/Eloquent/AdminRepository.php
+++ app/Repositories/Eloquent/AdminRepository.php
@@ -5,6 +5,7 @@
  */
 
 namespace App\Repositories\Eloquent;
+
 use Illuminate\Support\Facades\Request;
 
 /**
@@ -16,36 +17,37 @@ class AdminRepository extends BaseRepository
 {
     public function model()
     {
-       return "App\Models\Admin";
+        return "App\Models\Admin";
     }
 
     public function ajaxGetUsers($request)
     {
-        $draw = request('draw' , 1);
-        $start = request('start',0);
-        $length = request('length',10);
+        $draw = request('draw', 1);
+        $start = request('start', 0);
+        $length = request('length', 10);
         //排序
-        $order['name'] = $request->input('columns.' . $request->input('order.0.column').'.data');
-        $order['dir'] = $request->input('order.0.dir','asc');
+        $order['name'] = $request->input('columns.' . $request->input('order.0.column') . '.data');
+        $order['dir'] = $request->input('order.0.dir', 'asc');
 
         //搜索
 
-        $search['value'] = $request->input('search.value','');
-        $search['regex'] = $request->input('search.regex',false);
+        $search['value'] = $request->input('search.value', '');
+        $search['regex'] = $request->input('search.regex', false);
         $model = $this->makeModel();
         $totalRecords = $model::count();
-        if($search['value']){
-            $model = $model->where('name','like',"%{$search['value']}%");
+        if ($search['value']) {
+            $model = $model->where('name', 'like', "%{$search['value']}%");
         }
-        $data = $model->orderBy($order['name'],$order['dir'])->offset($start)->limit($length)->get()->toArray();
+        $data = $model->orderBy($order['name'], $order['dir'])->offset($start)->limit($length)->get()->toArray();
+
         return [
             //第几页
-            'draw'         =>$draw,
+            'draw'            => $draw,
             //总数量
-            'recordsTotal' => $totalRecords,
+            'recordsTotal'    => $totalRecords,
             //查询到的总数量
             'recordsFiltered' => count($data),
-            'data' =>$data
+            'data'            => $data
         ];
     }
 }
